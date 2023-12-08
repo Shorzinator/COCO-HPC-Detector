4161956d0ae254f91b1577236e3da79af5d642bf
Shorzinator <shorz2905@gmail.com>
2023-12-07 19:25:08 -0600

    Adding files to download test and val

---
+++ data/coco/images/val/download_coco_val.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define the URL for the COCO train dataset
+dataset_url="http://images.cocodataset.org/zips/val2014.zip"
+
+# Function to download and unzip data
+download_and_unzip() {
+    local url=$1
+    # shellcheck disable=SC2155
+    local zip_file=$(basename "$url")
+
+    # Download
+    echo "Downloading data from $url"
+    wget "$url"
+
+    # Unzip
+    echo "Unzipping the dataset"
+    unzip "$zip_file"
+    rm "$zip_file"  # Remove the zip file after extraction
+    echo "Download and extraction complete."
+}
+
+# Download and unzip the dataset in the current directory
+download_and_unzip $dataset_url
+
+echo "Dataset setup complete."
