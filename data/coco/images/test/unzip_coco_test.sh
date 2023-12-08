60d33ef5cab5fc9f49f9c50aa3ea0e4f71798ce7
Shorzinator <shorz2905@gmail.com>
2023-12-08 14:36:32 -0600

    Uploading scripts to unzip downloaded data

---
+++ data/coco/images/test/unzip_coco_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Define the name of the zip file
+zip_file="test2014.zip"
+
+# Function to unzip data
+unzip_data() {
+    local file=$1
+
+    # Check if the zip file exists
+    if [ -f "$file" ]; then
+        echo "Unzipping the file $file"
+        unzip "$file"
+        echo "Test Unzipping complete."
+    else
+        echo "Error: File $file not found."
+    fi
+}
+
+# Unzip the dataset zip file
+unzip_data $zip_file
+
